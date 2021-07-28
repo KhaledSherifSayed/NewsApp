864c5cffb9f1fbfd9ab5701dd5f9cebc4585c84d
Meslmawy <khaledsherif296@gmail.com>
2021-07-28 08:29:38 +0200

    remove unused codes format article dates

--- app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/ui/BindingAdapter.kt
+++ app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/ui/BindingAdapter.kt
@@ -2,13 +2,23 @@ package com.ibtikar.mvvm_starter_koin_coroutines.ui
 
 
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.databinding.BindingAdapter
 import coil.load
 import com.ibtikar.mvvm_starter_koin_coroutines.R
+import com.ibtikar.mvvm_starter_koin_coroutines.utils.DAY_MONTH_YEAR_HOUR_MIN
+import com.ibtikar.mvvm_starter_koin_coroutines.utils.YEAR_MONTH_DAY_T_TIME
+import com.ibtikar.mvvm_starter_koin_coroutines.utils.toDateFormatted
 
 @BindingAdapter("loadImage")
 fun loadImage(view: AppCompatImageView, url: String?) {
-    view.load(url){
+    view.load(url) {
         error(R.drawable.placeholder)
     }
+}
+
+
+@BindingAdapter("formatDate")
+fun formatArticleDate(view: AppCompatTextView, date: String?) {
+    view.text = date?.toDateFormatted(YEAR_MONTH_DAY_T_TIME, DAY_MONTH_YEAR_HOUR_MIN)
 }
--- app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/ui/onboarding/OnBoardingActivity.kt
@@ -1,49 +1,45 @@
 package com.ibtikar.mvvm_starter_koin_coroutines.ui.onboarding
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.chip.Chip
 import com.ibtikar.mvvm_starter_koin_coroutines.R
 import com.ibtikar.mvvm_starter_koin_coroutines.data.local.SharedPreferencesInterface
-import com.ibtikar.mvvm_starter_koin_coroutines.databinding.ActivityOnBoardingBinding
 import com.ibtikar.mvvm_starter_koin_coroutines.ui.MainActivity
-import com.ibtikar.mvvm_starter_koin_coroutines.ui.base.BaseActivity
-import com.ibtikar.mvvm_starter_koin_coroutines.ui.base.ViewState
 import com.ibtikar.mvvm_starter_koin_coroutines.utils.getKoinInstance
 import com.ibtikar.mvvm_starter_koin_coroutines.utils.intents.openActivity
 import kotlinx.android.synthetic.main.activity_on_boarding.*
-import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
+import kotlin.collections.ArrayList
 
 
-class OnBoardingActivity :
-    BaseActivity<ActivityOnBoardingBinding, OnBoardingViewModel>(R.layout.activity_on_boarding) {
+class OnBoardingActivity : AppCompatActivity() {
 
-    override val viewModel: OnBoardingViewModel by viewModel()
-    val categoriesFavList: ArrayList<String> = ArrayList()
-    val sharedPreferences by getKoinInstance<SharedPreferencesInterface>()
+    private val categoriesFavList: ArrayList<String> = ArrayList()
+    private val sharedPreferences by getKoinInstance<SharedPreferencesInterface>()
 
-    override fun setup() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
         initCategoriesChips()
         nextButton.setOnClickListener{
             sharedPreferences.categoriesFav = categoriesFavList.joinToString(separator = ",")
-            sharedPreferences.countryFav = ccp.selectedCountryNameCode
+            sharedPreferences.countryFav = ccp.selectedCountryNameCode.toLowerCase(Locale.ROOT)
             sharedPreferences.introduction = true
             openActivity(MainActivity::class.java, finish = true)
         }
     }
 
-    override fun render(state: ViewState) {
-
-    }
-
     private fun initCategoriesChips() {
         // Create a Chip for each regionsList item.
-        val chipGroup = binder.categoriesList
+        val chipGroup = categoriesList
         val inflator = LayoutInflater.from(chipGroup.context)
         val categories = arrayListOf<String>(*resources.getStringArray(R.array.Categories))
         val children = categories.map { categoryName ->
             val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
             chip.text = categoryName
-            chip.tag = categoryName.toLowerCase()
+            chip.tag = categoryName.toLowerCase(Locale.ROOT)
             chip
         }
         chipGroup.removeAllViews()
