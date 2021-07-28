484c60f7939b37ba308fd70ec24d2c1beed57f96
Meslmawy <khaledsherif296@gmail.com>
2021-07-28 17:08:26 +0200

    add search module , make search work locally

--- app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/ui/newsList/NewsListFragment.kt
+++ app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/ui/newsList/NewsListFragment.kt
@@ -3,16 +3,22 @@ package com.ibtikar.mvvm_starter_koin_coroutines.ui.newsList
 
 import android.content.Intent
 import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
+import com.google.android.material.chip.Chip
 import com.ibtikar.mvvm_starter_koin_coroutines.R
 import com.ibtikar.mvvm_starter_koin_coroutines.data.local.SharedPreferencesInterface
 import com.ibtikar.mvvm_starter_koin_coroutines.data.local.toFavoriteArticleEntity
+import com.ibtikar.mvvm_starter_koin_coroutines.data.models.NewsModelResponse
 import com.ibtikar.mvvm_starter_koin_coroutines.databinding.NewsListFragmentBinding
 import com.ibtikar.mvvm_starter_koin_coroutines.ui.base.BaseFragmentWithBusiness
 import com.ibtikar.mvvm_starter_koin_coroutines.ui.base.ViewState
-import com.ibtikar.mvvm_starter_koin_coroutines.utils.getKoinInstance
+import com.ibtikar.mvvm_starter_koin_coroutines.utils.*
 import kotlinx.android.synthetic.main.news_list_fragment.*
+import kotlinx.android.synthetic.main.toolbar_home.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
 
 
 /**
@@ -24,7 +30,8 @@ class NewsListFragment :
 
     private var articlesAdapter: ArticlesAdapter? = null
     val sharedPreferences by getKoinInstance<SharedPreferencesInterface>()
-
+    private var searchCategoriesFavList: ArrayList<String> = ArrayList()
+    private var newsList = mutableListOf<NewsModelResponse>()
     override val viewModel: NewsViewModel by viewModel()
 
     override fun setup() {
@@ -33,6 +40,30 @@ class NewsListFragment :
             sharedPreferences.countryFav,
             sharedPreferences.categoriesFav.split(",")
         )
+        searchIV.setOnClickListener {
+            hide(toolbar_title, settingsIV, favoriteIV, searchIV)
+            show(backImage, searchResultsET, searchCategoriesScrollview)
+        }
+
+        backImage.setOnClickListener {
+            show(toolbar_title, settingsIV, favoriteIV, searchIV)
+            hide(backImage, searchResultsET, searchCategoriesScrollview)
+            viewModel.getMostSharedArticles(
+                sharedPreferences.countryFav,
+                sharedPreferences.categoriesFav.split(",")
+            )
+            searchCategoriesFavList.clear()
+            searchCategoriesFavList.addAll(sharedPreferences.categoriesFav.split(","))
+            initCategoriesChips()
+            searchResultsET.clear()
+        }
+
+        searchResultsET.afterTextChanged {
+            filter(it)
+        }
+
+        searchCategoriesFavList.addAll(sharedPreferences.categoriesFav.split(","))
+        initCategoriesChips()
     }
 
     private fun setupAdapter() {
@@ -49,19 +80,81 @@ class NewsListFragment :
     override fun render(state: ViewState) {
         when (state) {
             is NewsViewState.onNewsResponse -> {
-                articlesAdapter?.submitList(state.data?.toMutableList())
+                newsList.clear()
+                newsList.addAll(state.data?.toMutableList()!!)
+                if (searchResultsET.visibility == View.VISIBLE)
+                    filter(searchResultsET.text.toString())
+                else
+                    articlesAdapter?.submitList(newsList)
             }
             is NewsViewState.onAddingFavoriteResponse -> {
                 if (state.data == true)
                     Toast.makeText(
                         context,
-                        "Article Added Successfully to your favorite",
+                        getString(R.string.favorite_success_msg),
                         Toast.LENGTH_LONG
                     ).show()
                 else
-                    Toast.makeText(context, "Article added before", Toast.LENGTH_LONG).show()
+                    Toast.makeText(
+                        context,
+                        getString(R.string.favorite_check_msg),
+                        Toast.LENGTH_LONG
+                    ).show()
             }
         }
     }
 
+    private fun initCategoriesChips() {
+        // Create a Chip for each regionsList item.
+        val chipGroup = searchCategoriesChips
+        val inflator = LayoutInflater.from(chipGroup.context)
+        val categories = arrayListOf<String>(*resources.getStringArray(R.array.Categories))
+        val children = categories.map { categoryName ->
+            val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+            chip.text = categoryName
+            chip.tag = categoryName.toLowerCase(Locale.ROOT)
+            chip.isChecked = searchCategoriesFavList.contains(chip.tag)
+            chip
+        }
+        chipGroup.removeAllViews()
+        for (chip in children) {
+            chip.setOnCheckedChangeListener { _, checked ->
+                if (checked) {
+                    searchCategoriesFavList.add(chip.tag as String)
+                    refreshSearchedCategories()
+                } else {
+                    if (searchCategoriesFavList.size > 1) {
+                        searchCategoriesFavList.remove(chip.tag as String)
+                        refreshSearchedCategories()
+                    } else {
+                        chip.isChecked = true
+                        Toast.makeText(
+                            context,
+                            getString(R.string.search_check_msg),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+            }
+            chipGroup.addView(chip)
+        }
+    }
+
+    private fun refreshSearchedCategories() {
+        viewModel.getMostSharedArticles(
+            sharedPreferences.countryFav,
+            searchCategoriesFavList
+        )
+    }
+
+    private fun filter(text: String) {
+        val filteredList: ArrayList<NewsModelResponse> = ArrayList()
+        for (item in newsList) {
+            if (item.title.toLowerCase(Locale.ROOT).contains(text.toLowerCase(Locale.ROOT))) {
+                filteredList.add(item)
+            }
+        }
+        articlesAdapter?.submitList(filteredList.toMutableList())
+    }
 }
--- app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/utils/Extensions.kt
+++ app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/utils/Extensions.kt
@@ -1,5 +1,9 @@
 package com.ibtikar.mvvm_starter_koin_coroutines.utils
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import java.util.*
@@ -13,3 +17,44 @@ inline fun <reified T> getKoinInstance(): Lazy<T> {
     }
 }
 
+
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun show(vararg views: View) {
+    views.forEach { it.show() }
+}
+
+fun View.hide() {
+    visibility = View.GONE
+}
+
+fun hide(vararg views: View) {
+    views.forEach { it.hide() }
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+
+}
+
+fun invisible(vararg views: View) {
+    views.forEach { it.invisible() }
+}
+
+fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(editable: Editable?) {
+            afterTextChanged.invoke(editable.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+    })
+}
+
+
+fun EditText.clear() { text.clear() }
